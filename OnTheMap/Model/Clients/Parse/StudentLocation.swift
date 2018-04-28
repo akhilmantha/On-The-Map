6bff4abd4d75f611ccd2b3351f7f1bfa03a3bfe5
akhilmantha <akhilmantha@live.com>
2018-04-29 02:29:54 +0530

    studentlocation structure is created and the location is fetched

--- OnTheMap/Model/Clients/Parse/StudentLocation.swift
+++ OnTheMap/Model/Clients/Parse/StudentLocation.swift
@@ -1,4 +1,4 @@
-://
+//
 //  StudentLocation.swift
 //  OnTheMap
 //
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+struct StudentLocation {
+    let objectId: String?
+    let uniqueKey: String?
+    let firstName: String?
+    let lastName: String?
+    let mapString: String?
+    let mediaUrl: String?
+    let latitude: Double?
+    let longitude: Double?
+    let createdAt: Date?
+    let updatedAt: Date?
+    let acl: AnyObject?   // TODO_ check acl class
+    
+    init(dictionary: [String:AnyObject]) {
+        if let id = dictionary[ParseClient.JSONResponseKeys.ObjectId] as? String { objectId = id }
+        else {objectId = nil}
+        
+        if let key = dictionary[ParseClient.JSONResponseKeys.UniqueKey] as? String { uniqueKey = key }
+        else {uniqueKey = nil}
+        
+        if let fName = dictionary[ParseClient.JSONResponseKeys.FirstName] as? String {firstName = fName}
+        else {firstName = nil}
+        
+        if let lName = dictionary[ParseClient.JSONResponseKeys.LastName] as? String {lastName = lName}
+        else {lastName = nil}
+        
+        if let map = dictionary[ParseClient.JSONResponseKeys.MapString] as? String {mapString = map}
+        else {mapString = nil}
+        
+        if let url = dictionary[ParseClient.JSONResponseKeys.MediaURL] as? String {mediaUrl = url}
+        else {mediaUrl = nil}
+        
+        if let lat = dictionary[ParseClient.JSONResponseKeys.Latitude] as? Double {latitude = lat}
+        else {latitude = nil}
+        
+        if let long = dictionary[ParseClient.JSONResponseKeys.Longitude] as? Double {longitude = long}
+        else {longitude = nil}
+        acl = nil
+        
+        let formatter = DateFormatter()
+        let createdAtString = dictionary[ParseClient.JSONResponseKeys.CreatedAt] as! String
+        createdAt = formatter.dateFromApiString(createdAtString)
+        
+        let updatedAtString = dictionary[ParseClient.JSONResponseKeys.UpdatedAt] as! String
+        updatedAt = formatter.dateFromApiString(updatedAtString)
+    }
+    
+    static func locationsFromResults(_ results: [[String:AnyObject]]) -> [StudentLocation] {
+        var locations = [StudentLocation]()
+        
+        // iterate through array of dictionaries, each Location is a dictionary
+        for result in results {
+            locations.append(StudentLocation(dictionary: result))
+        }
+        return locations
+    }
+}
--- OnTheMap/Model/StudentModel.swift
+++ OnTheMap/Model/StudentModel.swift
@@ -9,4 +9,4 @@
 import Foundation
 import UIKit
 
-var studentLocations : [studentLocation] = []
+var studentLocations : [StudentLocation] = []
