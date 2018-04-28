fc603775e04f19c66187f29320ccd43f9608eb02
akhilmantha <akhilmantha@live.com>
2018-04-29 01:46:15 +0530

    RequestUtils added to handle the raw json

--- OnTheMap/Model/Clients/Parse/ParseClient.swift
+++ OnTheMap/Model/Clients/Parse/ParseClient.swift
@@ -26,11 +26,24 @@ class ParseClient : NSObject {
     
     func addParseHeader(request: URLRequest) -> URLRequest {
         var requestWithHeader = request
-        requestWithHeader.addValue(, forHTTPHeaderField: <#T##String#>)
+        requestWithHeader.addValue(ParseClient.HeaderKeys.ApplicationID, forHTTPHeaderField: "X-Parse-Application-Id")
+        requestWithHeader.addValue(ParseClient.HeaderKeys.ApiKey, forHTTPHeaderField: "X-Parse-API-Key")
         
-        
+        return requestWithHeader
     }
     
+    func addPostOrPutParseHeader (request: URLRequest) -> URLRequest{
+        var requestWithHeader = addParseHeader(request: request)
+        requestWithHeader.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        return requestWithHeader
+    }
     
+    //Mark: GET
+    
+    func taskForGETMethod(_ method:String, parameters:[String:AnyObject], completionHandlerForGET: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {
+        
+        //Build the URL, configure the request
+        let request = URLRequest(url: urlFromParameters(parameters as [String: AnyObject], withPathExtension: method, api: .Parse))
+    }
     
 }
