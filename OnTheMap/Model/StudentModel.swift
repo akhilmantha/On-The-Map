cd9ff36f4f30022630c74be85d7c813450f10a30
akhilmantha <akhilmantha@live.com>
2018-05-03 00:07:30 +0530

    StudentModel modified

--- OnTheMap/Model/StudentModel.swift
+++ OnTheMap/Model/StudentModel.swift
@@ -6,7 +6,41 @@
 //  Copyright © 2018 akhil mantha. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-var studentLocations : [StudentLocation] = []
+class StudentModel {
+    
+    var studentsLocations : [StudentLocation] = []
+    var userLocation : StudentLocation? = nil
+    static let sharedInstance = StudentModel()
+    
+    func updateStudentsLocations(controller: UIViewController) {
+        if let viewController = controller as? FetchData {
+            viewController.fetchStarts()
+        }
+        
+        let _ = ParseClient.sharedInstance().getStudentsLocations() { (locations, error) in
+            
+            if let error = error {
+                print("There was an error at getStudentsLocations: \(error)")
+                controller.showSimpleAlert(text:error.localizedDescription)
+            }
+            
+            if let locations = locations {
+                StudentModel.sharedInstance.studentsLocations = locations
+                if let viewController = controller as? RefreshData {
+                    viewController.refresh()
+                }
+                print("updateStudentsLocations has finished successfully")
+            }
+            
+            if let viewController = controller as? FetchData {
+                viewController.fetchEnds()
+            }
+        }
+    }
+    
+    func updateStudentLocation(_ location: StudentLocation?) {
+        userLocation = location
+    }
+}
