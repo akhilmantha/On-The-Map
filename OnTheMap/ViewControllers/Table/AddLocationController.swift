bc3d434a773402ab840c5493fc602171cd15a34b
akhilmantha <akhilmantha@live.com>
2018-05-03 00:00:12 +0530

    AddLocationController updated

---
+++ OnTheMap/ViewControllers/Table/AddLocationController.swift
@@ -0,0 +1,110 @@
+//
+//  AddLocationController.swift
+//  OnTheMap
+//
+//  Created by akhil mantha on 02/05/18.
+//  Copyright © 2018 akhil mantha. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class AddLocationController: UIViewController {
+    
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var websiteTextField: UITextField!
+    @IBOutlet var stackView: UIStackView!
+    @IBOutlet var findLocationButton: UIButton!
+    @IBOutlet var contentView: UIView!
+    
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationTextField.delegate = self
+        websiteTextField.delegate = self
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        keyboardYLimit.buttonY = (findLocationButton?.frame.origin.y)! + stackView.frame.origin.y + contentView.frame.origin.y
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeToKeyboardNotifications()
+    }
+    
+    
+    // MARK: Actions
+    @IBAction func userDidTapView(_ sender: Any) {
+        resignIfFirstResponder(locationTextField)
+        resignIfFirstResponder(websiteTextField)
+    }
+    
+    @IBAction func findLocationButtonPressed(_ sender: Any) {
+        if locationTextField.text!.isEmpty ||
+            websiteTextField.text!.isEmpty {
+            self.showSimpleAlert(text:"Location or Website Empty.")
+        }
+        else {
+            view.showBlurLoader()
+            getCoordinatesForAddress(address: locationTextField.text!, vc: self, { (result, error) in
+                
+                DispatchQueue.main.async {
+                    if let error = error {
+                        
+                        self.showSimpleAlert(text: error.localizedDescription)
+                    }
+                        
+                    else if let result = result,
+                        let location = result.location,
+                        let name = result.name,
+                        let country = result.country {
+                        self.navigateToLocationPreview(location: location, name: name, country: country)
+                    }
+                    
+                    self.view.removeBlurLoader()
+                }
+                
+            })
+        }
+    }
+    
+    private func navigateToLocationPreview (location: CLLocation, name: String, country: String) {
+        if let navController = navigationController {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "AddLocationMapController") as! AddLocationMapController
+            vc.userLocationUpdate = AddLocationMapController.UserLocationUpdate(
+                userLocation: location,
+                userAddress: getCompleteAddressString(name: name, country: country),
+                userMediaUrl: websiteTextField.text!,
+                userName: getCompleteUserName())
+            
+            navController.pushViewController(vc, animated: true)
+        }
+    }
+    
+    private func getCompleteAddressString (name: String, country: String) ->String {
+        var completeAddress = ""
+        if name != "" {
+            completeAddress = name
+        }
+        if country != "" {
+            completeAddress += ", " + country
+        }
+        return completeAddress
+    }
+    
+    private func getCompleteUserName () -> String {
+        if let firstName  = StudentModel.sharedInstance.userLocation?.firstName,
+            let lastName = StudentModel.sharedInstance.userLocation?.lastName {
+            return firstName + ", " + lastName
+        }
+        else {
+            return "Unknown name"
+            
+        }
+    }
+}
+
