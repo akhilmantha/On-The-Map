5ee3cc3f04a2769bc4fd4610f996980b2f914ac1
akhilmantha <akhilmantha@live.com>
2018-05-03 00:12:31 +0530

    tableController updated

--- OnTheMap/ViewControllers/Table/TableController.swift
+++ OnTheMap/ViewControllers/Table/TableController.swift
@@ -10,4 +10,43 @@ import UIKit
 
 //Mark: TableViewController: UIViewController
 
+class TableController: UIViewController, RefreshData {
+    
+    // MARK: Properties
+    var studentsLocations: [StudentLocation] = [StudentLocation]()
+    private let refreshControl = UIRefreshControl()
+    @IBOutlet var tableView: UITableView!
+    
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let os = ProcessInfo().operatingSystemVersion
+        if (os.majorVersion <= 9) {
+            // remove top space
+            tableView.contentInset = UIEdgeInsetsMake(-44, 0, 0, 0);
+        }
+        addPullRefresh(tableView: tableView, refreshControl: refreshControl)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        studentsLocations = StudentModel.sharedInstance.studentsLocations
+        tableView.reloadData()
+    }
+    
+    // MARK: RefreshData
+    func refresh() {
+        studentsLocations = StudentModel.sharedInstance.studentsLocations
+        performUIUpdatesOnMain {
+            self.tableView.reloadData()
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
+    // MARK: UI
+    @objc func fetchTableData(_ sender: Any) {
+        StudentModel.sharedInstance.updateStudentsLocations(controller:self)
+    }
+}
 
