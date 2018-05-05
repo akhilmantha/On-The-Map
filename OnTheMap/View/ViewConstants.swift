0270206ea4e4816872b8a05af8c6dacde70f40ab
akhilmantha <akhilmantha@live.com>
2018-05-05 17:24:52 +0530

    viewConstants added to get the udacity color values

---
+++ OnTheMap/View/ViewConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ViewConstants.swift
+//  OnTheMap
+//
+//  Created by akhil mantha on 05/05/18.
+//  Copyright © 2018 akhil mantha. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let udacityBlue = UIColor(red: 2.0/255.0, green: 180.0/255.0, blue: 228.0/255.0, alpha: 1.0)
+    
+    
+}
