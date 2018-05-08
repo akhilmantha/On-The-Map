d47cc9334b1ada730246dbcf14f1d4a77a8b078d
akhilmantha <akhilmantha@live.com>
2018-05-08 12:39:18 +0530

    Deployment target changed

--- OnTheMap/Model/Clients/Udacity/UdacityClientUtils.swift
+++ OnTheMap/Model/Clients/Udacity/UdacityClientUtils.swift
@@ -8,28 +8,25 @@
 
 import Foundation
 
-extension UdacityClient{
+extension UdacityClient {
     
-    // replaces the key for the value that is contained in the method name
-    
-    func substitueKeyInJson(_ jsonPattern: String, key: String, value: String) -> String?{
+    // Replaces the key for the value that is contained within the method name
+    func substituteKeyInJson(_ jsonPattern: String, key: String, value: String) -> String? {
         if jsonPattern.range(of: "{\(key)}") != nil {
-            
             return jsonPattern.replacingOccurrences(of: "{\(key)}", with: value)
         }
         return nil
-        }
-
-    // Returns a json from a swift dictionary
-    func jsonFromDictionary(_ dictionary:[String: AnyObject]) -> Data {
-        var jsonData : Data! = nil
+    }
+    
+    // Returns a json froma a swift Dictionary
+    func jsonFromDictionary(_ dictionary: [String : AnyObject]) -> Data {
+        var jsonData: Data! = nil
         do {
             jsonData = try JSONSerialization.data(withJSONObject: dictionary)
             return jsonData
         } catch {
-            print("could not get json from dictionary: \(dictionary)")
+            print("Could not get josn from dictionary: '\(dictionary)'")
         }
         return Data()
     }
-    
 }
